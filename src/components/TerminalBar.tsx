a4af5fb597992cb1fce051ca226ffc003016ec8d
~Kanna! <mskannan1612@gmail.com>
2026-06-25 08:49:10 +0530

    Update TerminalBar.tsx

--- src/components/TerminalBar.tsx
+++ src/components/TerminalBar.tsx
@@ -10,7 +10,7 @@ export const TerminalBar: React.FC = () => {
       className={`relative w-full h-[50px] bg-ink bg-grid-dark flex flex-col sm:flex-row sm: justify-content gap-2 text-center px-6 text-white overflow-hidden select-none reveal-hidden ${isIntersecting ? 'reveal-visible' : ''}`}
     >
       {/* Left: status + blinking cursor */}
-      <div className="inline-flex item-center whitespace-nowrap font-mono text-center text-[12px]">
+      <div className="inline-flex whitespace-nowrap font-mono text-center text-[12px]">
         <span>&gt; status: open to internships / Jobs</span>
         <span className="ml-1 w-[6px] h-[1em] bg-white animate-cursor-blink" />
       </div>
